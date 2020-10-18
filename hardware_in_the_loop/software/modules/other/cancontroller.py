f1e0823d57640c7ee959b248019a907696346085
Alex Wenstrup <awenstrup@olin.edu>
2020-10-18 11:16:45 -0400

    Add to setup, keep developing cancontroller

--- hardware_in_the_loop/software/modules/other/cancontroller.py
+++ hardware_in_the_loop/software/modules/other/cancontroller.py
@@ -1,3 +1,8 @@
+import threading
+import can
+import time
+import os
+
 class CANController:
     """High level python object to interface with hardware.
 
@@ -10,10 +15,17 @@ class CANController:
         Args:
             ecus: dict of (str: ECU) pairs
         """
+        os.system("sudo ip link set can0 up type can bitrate 500000 restart-ms 100")  # bring can hardware online
+
         self.ecus = ecus
         self.read_dict = {}  # Dictionary to help translate raw can to useful signals
-        self.get_states()
-        # TODO start listening
+        self.get_states(can_spec_path)
+
+        # start listening
+        bus_name = "can0"  # set by `ip link set can0 up` from shell, see socketcan setup docs
+        can_bus = can.interface.Bus(bus_name, bus_type='socketcan')
+        listener = threading.Thread(target=listen, name="listener", kwargs={"can_bus": can_bus, "callback": update_ecu})
+        listener.start()
 
     def get_states(self, path: str):
         """Populate each ECUs `states` dictionary, and self.read_dict
@@ -41,9 +53,34 @@ class CANController:
         """Given a raw can message, generate a dictionary of values to update
 
         Args:
-            message: TODO define this
+            message: can.Message object. Notable attributes include
+                - timestamp
+                - arbitration_id
+                - data
+                - see https://python-can.readthedocs.io/en/master/message.html#can.Message
+
+        Returns:
+            (str, dict): The ECU name as a str, and a dict of values to update
+
         """
         raise Exception("Not Implemented!")
+
+
+    def listener(**kwargs):
+        """Thread that runs all the time to listen to CAN messages
+        
+        References:
+          - https://python-can.readthedocs.io/en/master/interfaces/socketcan.html
+          - https://python-can.readthedocs.io/en/master/
+        """
+        can_bus = kwargs["can_bus"]
+        callback = kwargs["callback"]
+        
+        while True:
+            msg = can_bus.recv()  # No timeout
+            callback() # TODO
+
+
 
 
         
