87506ae4620b5340a436ba475584aa96fabd6ca9
Manu Patil <mpatil99@gmail.com>
2020-11-22 17:10:07 -0500

    Save place

--- firmware_revamp/can_api/megayaml_generator.py
+++ firmware_revamp/can_api/megayaml_generator.py
@@ -1,14 +1,18 @@
 import pandas as pd 
 from pprint import pprint
+import yaml
 
 data = pd.read_csv("mkv_can_address_space.csv") 
 
 # print(data.head())
-# lets start by just extracting the info thats there
+# lets start by just extracting the info thats therews
 output = {}
 for index, row in data.iterrows():
-    if row < 13:
+    if index < 13:
         continue
+    if index > 14:
+        break
+    print(row)
     row_out = {}
     row_out["can_id"] = row["CAN ID"]
     row_out["dec"] = row["(dec)"]
@@ -21,13 +25,38 @@ for index, row in data.iterrows():
     for i in range(8):
         signal_names.append(row[f'Byte {i}'])
     row_out['signal_names'] = signal_names
-    
+    # print(signal)
 
-    sending_board = row["Sending Board"]
+    row_out["sending_board"] = row["Sending Board"]
 
     output[hex(int(row_out["dec"]))] = row_out
     
-    if index > 16:
-        break
+    message = {}
+    message["id"] = row_out["dec"]
+    message['data_bytes'] = row_out["len"]
+    message['signals'] = {}
+
+    for name in row_out['signal_names']:
+        signal = {}
+        signal["length"] = 8
+        signal["scale"] = 10
+        signal["offset"] = 0
+        signal["min"] = 0
+        signal["max"] = 1
+        signal["unit"] = "bit"
+        
+        message['signals'][name] = signal
+
     
-pprint(output)
+    yaml_out = {
+        "MessagesTX": {
+            row_out["sending_board"]: message
+
+        }
+    }
+
+
+ 
+    print(yaml.dump(yaml_out))
+
+    
