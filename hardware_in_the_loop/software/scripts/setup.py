711e179f068371da35533bea37b52bc548e3797a
Alex Wenstrup <awenstrup@olin.edu>
2020-11-28 11:22:26 -0500

    Don't change .bashrc if we already have

--- hardware_in_the_loop/software/scripts/setup.py
+++ hardware_in_the_loop/software/scripts/setup.py
@@ -11,17 +11,23 @@ def bash(s: str):
 if "linux" not in sys.platform:
     raise Exception("This script only works on Linux! For help with setup on another os, contact awenstrup@olin.edu")
 
-# Add project to PYTHONPATH
-# TODO can we replace this with `pip install -e {project_file_path}`???
+# Get paths
 this_file_path = os.path.abspath(__file__)
 project_file_path = os.path.abspath(os.path.join(this_file_path, os.pardir, os.pardir))
 
-print(f"Adding {project_file_path} to the PYTHONPATH...")
-with open("~/.bashrc", "a") as f:
-    f.write("\n----Added by OEM Advanced Research HITL system----\n")
-    f.write(f"export PYTHONPATH=$PYTHONPATH:{project_file_path}\n")
-    f.write("--------------------------------------------------\n")
+# Add project to PYTHONPATH
+with open("~/.bashrc", "r") as f:
+    data = f.read()
 
+if f"export PYTHONPATH=$PYTHONPATH:{project_file_path}" in data:
+    print("Project already in PYTHONPATH, moving on...")
+else:
+    print(f"Adding {project_file_path} to the PYTHONPATH...")
+    with open("~/.bashrc", "a") as f:
+        f.write("\n----Added by OEM Advanced Research HITL system----\n")
+        f.write(f"export PYTHONPATH=$PYTHONPATH:{project_file_path}\n")
+        f.write("--------------------------------------------------\n")
+    
 # Install python requirements
 print(f"Installing python requirements from {project_file_path}/requirements.txt ...")
 bash(f"pip install -r {project_file_path}/requirements.txt")
