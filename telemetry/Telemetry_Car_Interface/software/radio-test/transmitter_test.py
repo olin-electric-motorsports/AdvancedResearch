7079ea8c3fa2283e848effa8b99e5f04ab586fd2
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-03-09 02:02:26 -0500

    Update usb port in transmitter code

--- telemetry/Telemetry_Car_Interface/software/radio-test/transmitter_test.py
+++ telemetry/Telemetry_Car_Interface/software/radio-test/transmitter_test.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial(port='/dev/ttyUSB2',baudrate=57600)
+ser = serial.Serial(port='/dev/ttyUSB0',baudrate=57600)
 while True:
     ser.write(b'Hi There!')
     time.sleep(2.0)
