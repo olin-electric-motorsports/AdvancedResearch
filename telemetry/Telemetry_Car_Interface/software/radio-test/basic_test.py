1efd5f57831c9fcb8c6e446203467d5cbc8c5a24
Luke Nonas-Hunter <lukenonashunter@gmail.com>
2021-03-08 23:51:58 -0500

    Write basic test

---
+++ telemetry/Telemetry_Car_Interface/software/radio-test/basic_test.py
@@ -0,0 +1,12 @@
+import serial
+
+def send(s):
+    """
+    Send a UART message over GPIO14 and GPIO15 of the raspberrypi
+    """
+    s.write("AAA")
+
+
+if __name__ == "__main__":
+    ser = serial.Serial ("/dev/ttyS0", 9600) 
+    send(ser)
